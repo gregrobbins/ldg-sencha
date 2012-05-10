548d897966ad9f2a5dd4db637ae48e22ad43995d
Greg Robbins <greg.pwpp@gmail.com>
2012-05-10 09:20:10 +0200

    added marker for LDG on map

--- app/view/Map.js
+++ app/view/Map.js
@@ -10,6 +10,20 @@ Ext.define('ldg.view.Map', {
             center: new google.maps.LatLng(41.394833,2.148579),
             MapTypeId: google.maps.MapTypeId.ROADMAP,
             zoom: 16,
-        }
+        },
+        listeners: [
+            {
+                fn: 'onMapRender',
+                event: 'maprender'
+            }
+        ]
+    },
+
+    onMapRender: function (map, gmap, options) {
+        //console.log('map rendered');
+        new google.maps.Marker({
+            position: new google.maps.LatLng(41.394833,2.148579),
+            map: gmap
+        });
     }
 });
